606cddb84b1c39d0cd76d11bb218d738a69b4731
eomur <eomur@DESKTOP-73L7CKH>
2026-06-16 17:59:17 +0300

    Fix SprintHost deploy settings

--- fit_service/wsgi.py
+++ fit_service/wsgi.py
@@ -1,4 +1,14 @@
 import os
+import sys
+
+try:
+    import pysqlite3
+    sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
+except ImportError:
+    pass
+
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fit_service.settings.settings')
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fit_service.settings')
+
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python
-import os, sys
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+import os
+import sys
+
+try:
+    import pysqlite3
+    sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
+except ImportError:
+    pass
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fit_service.settings')
+
 from django.core.management import execute_from_command_line
+
 execute_from_command_line(sys.argv)
